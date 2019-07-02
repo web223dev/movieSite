777d2520c907a5fd2eb28540bd6dcd0b5acf3b78
web223dev <ruslanus866@outlook.com>
2019-07-03 00:55:01 +0800

    Changed Episodes Style

--- src/components/MovieSection/index.js
+++ src/components/MovieSection/index.js
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom';
 const MovieSection = ({ mdetail, bgImg, pagename, category }) => {
     return (
         <div className="movie-section">
-            <Link to={category=== "tv" ? `/tv/${mdetail.id}` : `/movie/${mdetail.id}`} className="movielink">
+            <Link to={category === "tv" ? `/tv/${mdetail.id}` : `/movie/${mdetail.id}`} className="movielink">
                 {/* <img src={bgImg === undefined ? pagename === 'movieDetail' ? 'http://via.placeholder.com/255x450' : 'http://via.placeholder.com/450x255' : bgImg} alt={mdetail.title} height={pagename === 'movieDetail' ? 298 : 168 } /> */}
-                <img src={ pagename === 'movieDetail' ? 'http://via.placeholder.com/300x450' : 'http://via.placeholder.com/450x255'} alt={mdetail.title} height={pagename === 'movieDetail' ? 298 : 168 } />
-                <span className="episodes">20 Episodes</span>
+                <img src={pagename === 'movieDetail' ? 'http://via.placeholder.com/300x450' : 'http://via.placeholder.com/450x255'} alt={mdetail.title} height={pagename === 'movieDetail' ? 298 : 168} />
+                {(pagename === 'movieDetail') && <span className="episodes">20 Episodes</span>}
             </Link>
         </div>
     );
