cb7156a500f255a7273a2f211565f609ee8b52cc
web223dev <ruslanus866@outlook.com>
2019-07-04 17:49:10 +0800

    Problem 1 fixed

--- src/containers/Home/SliderSettings.js
+++ src/containers/Home/SliderSettings.js
@@ -1,47 +1,50 @@
 const settings = {
+    className: "slider variable-width",
     infinite: false,
-    speed: 700,
-    slidesToScroll: 6,
-    slidesToShow: 6,
-    responsive: [
-        {
-            breakpoint: 1900,
-            settings: {
-                slidesToShow: 5,
-                slidesToScroll: 5,
-            }
-        },
-        {
-            breakpoint: 1620,
-            settings: {
-                slidesToShow: 4,
-                slidesToScroll: 4,
-            }
-        },
-        {
-            breakpoint: 1310,
-            settings: {
-                slidesToShow: 3,
-                slidesToScroll: 3,
-            }
-        },
-        {
-            breakpoint: 1010,
-            settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2,
+    arrows: false,
+    // speed: 700,
+    slidesToScroll: 1,
+    slidesToShow: 1,
+    variableWidth: true,
+    // responsive: [
+    //     {
+    //         breakpoint: 1900,
+    //         settings: {
+    //             slidesToShow: 5,
+    //             slidesToScroll: 5,
+    //         }
+    //     },
+    //     {
+    //         breakpoint: 1620,
+    //         settings: {
+    //             slidesToShow: 4,
+    //             slidesToScroll: 4,
+    //         }
+    //     },
+    //     {
+    //         breakpoint: 1310,
+    //         settings: {
+    //             slidesToShow: 3,
+    //             slidesToScroll: 3,
+    //         }
+    //     },
+    //     {
+    //         breakpoint: 1010,
+    //         settings: {
+    //             slidesToShow: 2,
+    //             slidesToScroll: 2,
 
-            }
-        },
-        {
-            breakpoint: 720,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
+    //         }
+    //     },
+    //     {
+    //         breakpoint: 720,
+    //         settings: {
+    //             slidesToShow: 1,
+    //             slidesToScroll: 1
 
-            }
-        }
-    ]
+    //         }
+    //     }
+    // ]
 };
 
 // const settings = {
