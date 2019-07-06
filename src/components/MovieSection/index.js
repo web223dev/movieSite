08e4aa0acd0c3db63563e8c5f067c294e3900838
web223dev <ruslanus866@outlook.com>
2019-07-07 07:19:30 +0800

    changed images

--- src/components/MovieSection/index.js
+++ src/components/MovieSection/index.js
@@ -5,9 +5,9 @@ const MovieSection = ({ mdetail, bgImg, pagename, category, isLoading }) => {
     return (
         <div className="movie-section">
             <Link to={category === "tv" ? `/tv/${mdetail.id}` : `/movie/${mdetail.id}`} className="movielink">
-                <img src={bgImg === undefined ? pagename === 'movieDetail' ? 'http://via.placeholder.com/255x450' : 'http://via.placeholder.com/450x255' : bgImg} alt={mdetail.title} height={pagename === 'movieDetail' ? 298 : 168 } />
+                {/* <img src={bgImg === undefined ? pagename === 'movieDetail' ? 'http://via.placeholder.com/255x450' : 'http://via.placeholder.com/450x255' : bgImg} alt={mdetail.title} height={pagename === 'movieDetail' ? 298 : 168 } /> */}
                 {/* <img src={bgImg === undefined ? pagename === 'movieDetail' ? 'http://via.placeholder.com/255x450' : 'http://via.placeholder.com/450x255' : bgImg} alt={mdetail.title} /> */}
-                {/* <img src={pagename === 'movieDetail' ? 'http://via.placeholder.com/300x450' : 'http://via.placeholder.com/450x255'} alt={mdetail.title} height={pagename === 'movieDetail' ? 298 : 168} /> */}
+                <img src={pagename === 'movieDetail' ? 'http://via.placeholder.com/300x450' : 'http://via.placeholder.com/450x255'} alt={mdetail.title} height={pagename === 'movieDetail' ? 298 : 168} />
                 {/* <img src={pagename === 'movieDetail' ? 'http://via.placeholder.com/300x450' : 'http://via.placeholder.com/450x255'} alt={mdetail.title} /> */}
                 {(pagename === 'movieDetail') && <span className="episodes">20 Episodes</span>}
             </Link>            
